0ce544ccdd9846c51026624266fe0021321c3730
caaavilato <caaavilato@unal.edu.co>
2016-11-13 18:55:14 -0500

    Update
    update

--- Game/core/src/com/mygdx/game/Player.java
+++ Game/core/src/com/mygdx/game/Player.java
@@ -6,6 +6,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -22,80 +23,146 @@ import com.badlogic.gdx.utils.Array;
  * @author Camilo.Avila
  */
 class Player extends Sprite {
-
+      
       private PlayScreen screen;
       
       public World world;
       public Body b2body;
       
       private TextureRegion stand;
+      
       private Animation standing;
-      private Animation running;
+      private Animation standingLoop1;
+      private Animation standing2;
+      private Animation standingLoop2;
+      private Animation standing3;
       
       private float stateTimer;
       private boolean runningRight;
       
+      private Array<TextureRegion> frames;
+      private TextureRegion frame;
+      private float TimeState = 0;
       
-      public Player (PlayScreen Screen) {
-      
+      public Player(PlayScreen Screen) {
             
-      
-      this.screen = Screen;     
-      this.world = Screen.getWorld();
-      
-      Array<TextureRegion> frames = new Array<TextureRegion>();
-      
-      stand = new TextureRegion( screen.getAtlas().findRegion("PlayerStanding"), 0, 0, 32 , 64 );
-      System.out.println();
-      
-      definePlayer();
-      
-      setBounds(0, 0, 32/ AdventureGame.PPM, 64/ AdventureGame.PPM );
-      setRegion(stand);
+            this.screen = Screen;            
+            this.world = Screen.getWorld();
+            
+            frames = new Array<TextureRegion>();
+            
+            stand = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), 0, 0, 24, 62);
+            System.out.println();
+            
+            definePlayer();
+            
+            setBounds(0, 0, 16 * 1.3f / AdventureGame.PPM, 32 * 1.3f / AdventureGame.PPM);
+            setRegion(stand);
+            
+            for (int i = 1; i <= 3; i++) {
+                  frame = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), (27 * i) + 1, 0, 27, 62);                  
+                  
+                  frames.add(frame);
+            }
+            standing = new Animation(0.25f, frames);
+            frames.clear();
+            
+            for (int i = 4; i <= 5; i++) {
+                  frame = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), (27 * i) + 1, 0, 27, 62);                  
+                  
+                  frames.add(frame);
+            }
+            standingLoop1 = new Animation(0.7f, frames, PlayMode.LOOP);
+            frames.clear();
+            
+            for (int i = 6; i <= 11; i++) {
+                  frame = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), (27 * i) + 1, 0, 27, 62);                  
+                  
+                  frames.add(frame);
+            }
+            standing2 = new Animation(0.25f, frames);
+            frames.clear();
+            
+            for (int i = 12; i <= 14; i++) {
+                  frame = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), (27 * i) + 1, 0, 27, 62);                  
+                  
+                  frames.add(frame);
+            }
+            standingLoop2 = new Animation(0.3f, frames, PlayMode.LOOP);
+            frames.clear();
+            
+            for (int i = 15; i <= 17; i++) {
+                  frame = new TextureRegion(screen.getAtlas().findRegion("PlayerStanding"), (27 * i) + 1, 0, 27, 62);                  
+                  
+                  frames.add(frame);
+            }
+            standing3 = new Animation(0.4f, frames);
+            frames.clear();
       }
       
-      
-      
-      public void render(){
+      public void render() {
             
       }
       
-      public void draw(Batch batch){
+      public void draw(Batch batch) {
             
             super.draw(batch);
             
+      }
+      
+      public void update(float dt) {
+            
+            setPosition(b2body.getPosition().x - super.getWidth() / 2, b2body.getPosition().y - super.getHeight() / 2);
+            
+            getFrame(dt);
+      }
+      
+      public void getFrame(float dt) {
+            
+            TextureRegion region;
+            if (TimeState < standing.getAnimationDuration()) {
+                  region = standing.getKeyFrame(TimeState);
+            } else if (TimeState < (standing.getAnimationDuration() + (6 * standingLoop1.getAnimationDuration()))) {
+                  region = standingLoop1.getKeyFrame(TimeState - standing.getAnimationDuration());
+            } else if (TimeState < (standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration())) {
+                  region = standing2.getKeyFrame(TimeState - (standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration()));
+            } else if (TimeState < (standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration() + 5 * standingLoop2.getAnimationDuration()) ) {
+                  region = standingLoop2.getKeyFrame(TimeState - (standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration() ) );
+            } else if (TimeState < (standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration() + 5 * standingLoop2.getAnimationDuration()  + standing.getAnimationDuration()) ) {
+                  region = standing3.getKeyFrame(TimeState - ( standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration() + 5 * standingLoop2.getAnimationDuration()) );
+            } else{
+                  TimeState = standing.getAnimationDuration() + 6 * standingLoop1.getAnimationDuration() + standing2.getAnimationDuration() ;
+                  region = standingLoop2.getKeyFrame(0);
+                  
+            }
+                  
+                  
+            
+            setRegion(region);
+            TimeState += dt;
+            
             
       }
       
-      public void update(float dt){
-      
-            setPosition(b2body.getPosition().x - super.getWidth()/2, (b2body.getPosition().y + 30 / AdventureGame.PPM ) - super.getHeight()/2 );
+      public void definePlayer() {
             
-            setRegion(stand);
-      }
-      
-      private PolygonShape shape = new PolygonShape();
-      public void definePlayer(){
-           
+            PolygonShape shape = new PolygonShape();
+            
             BodyDef bdef = new BodyDef();
-            bdef.position.set( 64/ AdventureGame.PPM, 64/ AdventureGame.PPM);
+            bdef.position.set(64 / AdventureGame.PPM, 64 / AdventureGame.PPM);
             bdef.type = BodyDef.BodyType.DynamicBody;
             
             b2body = world.createBody(bdef);
             
             FixtureDef fdef = new FixtureDef();
             
-            shape.setAsBox(16 / AdventureGame.PPM, 16 / AdventureGame.PPM);
+            shape.setAsBox(8 / AdventureGame.PPM, 20 / AdventureGame.PPM);
             //CircleShape shape = new CircleShape();
-            //shape.setRadius(6 / AdventureGame.PPM);
+            //shape.setRadius(8 / AdventureGame.PPM);
             
             fdef.shape = shape;
             b2body.createFixture(fdef).setUserData(this);
             
-            
-            
       }
-      
-      
       
 }
